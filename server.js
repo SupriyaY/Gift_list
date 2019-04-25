5b4eb5577b13f57de35ce99b251e5e512217c4b4
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-04-25 14:54:35 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -17,7 +17,25 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/express-mongoose-lesson-starter', { useNewUrlParser: true })
 
 // Now that we're connected, let's save that connection to the database in a variable.
-const db = mongoose.connection
+// const db = mongoose.connection
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-mongoose-lesson-starter');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+})
+
+
+
 
 //Will log an error if db can't connect to MongoDB
 db.on('error', function (err) {
