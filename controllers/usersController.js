4687f3269a4af513e5a9d4fe74a31250d13f7fef
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-04-30 13:09:34 -0400

    not rendering show view, but returning object

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,7 @@ const Item = require('../models/item')
 router.get('/', (req, res) => {
     User.find()
         .then(users => {
-            console.log(users);
+            // console.log(users);
             res.render('users/index', {users});
         })
         .catch(err => {
@@ -17,7 +17,6 @@ router.get('/', (req, res) => {
 
 });
 
-// add USERS TO EVERYTHIN 
 // USER SHOW ROUTE
 router.get('/:id', (req, res) => {
     User.findById(req.params.id)
@@ -32,56 +31,58 @@ router.get('/:id', (req, res) => {
 });
 
 
-
+// router.get('/new', (req, res) => {
+//     res.render('users/new')
+// })
 // USER CREATE ROUTE
-router.post('/',(req, res) => {
-const newUser = req.body
-if (!newUser.photo_url) {
-    newUser.photo_url = 'https://i.imgur.com/GNbsodM.jpg?1'
-}
-User.create(newUser)
-.then(() => {
-  res.redirect('/users')
-})
-.catch((error) => {
-  console.log(error)
-})
-})
+// router.post('/',(req, res) => {
+// const newUser = req.body
+// if (!newUser.photo_url) {
+//     newUser.photo_url = 'https://i.imgur.com/GNbsodM.jpg?1'
+// }
+// User.create(newUser)
+// .then(() => {
+//   res.redirect('/')
+// })
+// .catch((error) => {
+//   console.log(error)
+// })
+// })
 
 
 // USER EDIT ROUTE
-router.get('/:id/edit', (req, res) => {
-    User.findById(req.params.id)
-    .then((err, user) => {
-        if(err){
-            return;
-        } 
-     res.send(user)
-    });
-});
+// router.get('users/:id/edit', (req, res) => {
+//     User.findById(req.params.id)
+//     .then((err, user) => {
+//         if(err){
+//             return;
+//         } 
+//      res.send(user)
+//     });
+// });
 
 
 //UPDATE
-    router.put('/:id', function(req, res) {
-        User.findByIdAndUpdate(req.params.id, req.body, {new: true})   
-        .then(()=> {
-          console.log(user);
-          res.send(users);
-        });
-      });
+    // router.put('/users/:id', function(req, res) {
+    //     User.findByIdAndUpdate(req.params.id, req.body, {new: true})   
+    //     .then(()=> {
+    //       console.log(user);
+    //       res.send(users);
+    //     });
+    //   });
       
 
 // USER DESTROY
 
-router.delete('/:id', (req, res) => {
-User.findOneAndRemove(request.params.id)
-.then (() => {
-    res.send(users)
-})
-.catch((error) => {
-    console.log(error)
-})
-});
+// router.delete('/users/:id', (req, res) => {
+// User.findOneAndRemove(request.params.id)
+// .then (() => {
+//     res.send(users)
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
+// });
 
 
 // USER UPDATE ROUTE
--- public/script.js
+++
@@ -1,6 +0,0 @@
-let feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'handbags',
-    clientId: 'YOUR_CLIENT_ID'
-});
-feed.run();
